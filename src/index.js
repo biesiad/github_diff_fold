347f561ffdeee0b29ad6800523ebe18d2d30d71a
Greg Biesiadecki <greg.biesiadecki@crowdflower.com>
2016-07-10 20:24:14 -0700

    Create buttons on pjax:success

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@
 
 var Elm = require('./fold_button.elm');
 
-var nodes = Array.prototype.slice.call(document.querySelectorAll('.file-actions'))
+var createFoldButtons = function () {
+  var nodes = Array.prototype.slice.call(document.querySelectorAll('.file-actions'))
 
-nodes.forEach(function (node) {
-  var foldOnInit = !!node.parentNode.getAttribute('data-path').match(/postcss\.css|bundle\.js/)
-  var foldButton = Elm.FoldButton.embed(node, foldOnInit)
+  nodes.forEach(function (node) {
+    var foldOnInit = !!node.parentNode.getAttribute('data-path').match(/postcss\.css|bundle\.js/)
+    var foldButton = Elm.FoldButton.embed(node, foldOnInit)
 
-  var display = function (value) {
-    var codeNode = this.parentNode.parentNode.querySelector('.blob-wrapper')
-    codeNode.style.display = value
-  }
+    var display = function (value) {
+      var codeNode = this.parentNode.parentNode.querySelector('.blob-wrapper')
+      codeNode.style.display = value
+    }
 
-  foldButton.ports.setDisplay.subscribe(display.bind(node))
-})
+    foldButton.ports.setDisplay.subscribe(display.bind(node))
+  })
+}
+
+document.addEventListener('pjax:success', createFoldButtons, false)
+createFoldButtons()
