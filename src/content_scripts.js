5646bf5d63499f06930267d3794034bd31e4e115
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2016-07-24 16:01:57 -0700

    Update JS side initialization

--- src/content_scripts.js
+++ src/content_scripts.js
@@ -2,16 +2,20 @@ require('file?name=./build/[name].[ext]!./manifest.json')
 
 var Elm = require('./FoldButton.elm');
 
-var createFoldButtons = function () {
+var createFoldButtons = function (regexes) {
   var nodes = Array.prototype.slice.call(document.querySelectorAll('.file-actions'))
 
   nodes.forEach(function (node) {
     var path = node.parentNode.getAttribute('data-path')
-    var foldOnInit = path.match(/postcss\.css|bundle\.js/) && !path.match(/shared/)
+
+    var foldOnInit = regexes.reduce(function (acc, regex) {
+      return (acc = acc || !!path.match(RegExp(regex)))
+    }, false)
+
     var codeBlob = node.parentNode.parentNode.querySelector('.blob-wrapper')
 
     if (codeBlob) {
-      var foldButton = Elm.FoldButton.embed(node, !!foldOnInit)
+      var foldButton = Elm.FoldButton.embed(node, foldOnInit)
       foldButton.ports.setDisplay.subscribe(function (value) {
         codeBlob.style.display = value
       })
@@ -19,5 +23,7 @@ var createFoldButtons = function () {
   })
 }
 
-document.addEventListener('pjax:success', createFoldButtons, false)
-createFoldButtons()
+chrome.storage.local.get({ values: [] }, function (data) {
+  document.addEventListener('pjax:success', createFoldButtons.bind(null, data.values), false)
+  createFoldButtons(data.values)
+})
--- src/options.js
+++ src/options.js
@@ -2,4 +2,12 @@ require('file?name=./build/[name].[ext]!./options.html')
 require('file?name=./build/[name].[ext]!./icon.png')
 
 var Elm = require('./Options.elm');
-Elm.Options.embed(document.body, [])
+
+
+chrome.storage.sync.get({ values: [] }, function (data) {
+  var options = Elm.Options.embed(document.body, data.values);
+
+  options.ports.save.subscribe(function (values) {
+    chrome.storage.sync.set({ values: values.filter(function (v) { return v !== "" }) });
+  });
+});
