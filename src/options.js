f0f596c87ad7690b4012b816a3952ffd5b59ee79
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2016-07-24 16:01:06 -0700

    Add Icon

--- src/options.js
+++ src/options.js
@@ -1,4 +1,5 @@
 require('file?name=./build/[name].[ext]!./options.html')
+require('file?name=./build/[name].[ext]!./icon.png')
 
 var Elm = require('./Options.elm');
 Elm.Options.embed(document.body, [])
